0ae4ec679e00d6e28f4642bc48db941b5c512b84
Rayna McGinnis <raynamcginnis@Raynas-MacBook-Pro.local>
2024-05-28 12:22:56 -0600

    styles added to plugin front end

--- FFWS-custom-block-contact.php
+++ FFWS-custom-block-contact.php
@@ -8,9 +8,17 @@
 
  /** registers function in js */
  function ffws_custom_block_script_register() {
-    wp_enqueue_script('ffws-custom-block', plugin_dir_url(__FILE__).'ffws-block.js', array('wp-blocks', 'wp-i18n', 'wp-editor'), true, false);
+        wp_enqueue_script('ffws-custom-block', plugins_url('ffws-block.js', __FILE__), array('wp-blocks', 'wp-i18n', 'wp-editor'), true, false
+        );
  }
  add_action('enqueue_block_editor_assets', 'ffws_custom_block_script_register' );
 
+ function custom_block_styles() {
+ 
+    // enqueue style
+    wp_enqueue_style( 'ffws-block-styles', plugin_dir_url(__FILE__). 'ffws-block.css' );                       
+   }
+   
+   add_action( 'wp_enqueue_scripts', 'custom_block_styles');
 
 ?>
--- readme.txt
+++ readme.txt
@@ -1,13 +1,22 @@
-# Company Contact Information Block
-## Custom Gutenberg Block
+#Company Contact Information Block
+##Custom Gutenberg Block
 
-### Description
+###Description
 This custom Gutenberg block includes the company name, email address, phone number and address.
 
-### Technologies Used
+###Technologies Used
 - PHP
 - React.js
 
-
-### Tools Used
+###Tools Used
 - Babel
+
+###Screen Shots
+Activation Screen
+FFWS-custom-block-contact/img/plugin-activation.png
+
+View of the plugin options from the editor
+FFWS-custom-block-contact/img/backend-view.png
+
+View of how the plugin displays on the front end.
+FFWS-custom-block-contact/img/front-end-view.png
